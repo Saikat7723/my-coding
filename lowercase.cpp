4fd45d266fb7cf36683c9536cb6b57323fad84b5
Saikat Manna <119998924+Saikat7723@users.noreply.github.com>
2024-09-21 11:41:36 +0530

    Add files via upload

---
+++ lowercase.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch ;
+    cout<<"Enter char:";
+    cin>>ch;
+    if(ch >='a'&&ch <='z'){ 
+        cout<<"lowercase";
+    }else{
+        cout<<"uppercase";
+    }
+    return 0;
+}
